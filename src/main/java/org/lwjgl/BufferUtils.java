cb9d887586039b168d163bbb9ef3b742102db30e
PeytonPlayz595 <106421860+PeytonPlayz595@users.noreply.github.com>
2023-11-12 00:33:22 +0000

    Update BufferUtils

--- src/main/java/org/lwjgl/BufferUtils.java
+++ src/main/java/org/lwjgl/BufferUtils.java
@@ -2,16 +2,49 @@ package org.lwjgl;
 
 import java.nio.*;
 
-public class BufferUtils {
-	public static FloatBuffer createFloatBuffer(int i1) {
-		return FloatBuffer.wrap(new float[i1]);
-	}
-	
-	public static IntBuffer createIntBuffer(int i1) {
-		return IntBuffer.wrap(new int[i1]);
-	}
-	
+public class BufferUtils {	
 	public static ByteBuffer createByteBuffer(int i1) {
 		return ByteBuffer.wrap(new byte[i1]).order(ByteOrder.nativeOrder());
+	}
+
+	public static ShortBuffer createShortBuffer(int size) {
+		return createByteBuffer(size << 1).asShortBuffer();
+	}
+
+	public static CharBuffer createCharBuffer(int size) {
+		return createByteBuffer(size << 1).asCharBuffer();
+	}
+
+	public static IntBuffer createIntBuffer(int size) {
+		return createByteBuffer(size << 2).asIntBuffer();
+	}
+
+	public static LongBuffer createLongBuffer(int size) {
+		return createByteBuffer(size << 3).asLongBuffer();
+	}
+
+	public static FloatBuffer createFloatBuffer(int size) {
+		return createByteBuffer(size << 2).asFloatBuffer();
+	}
+
+	public static DoubleBuffer createDoubleBuffer(int size) {
+		return createByteBuffer(size << 3).asDoubleBuffer();
+	}
+
+	public static int getElementSizeExponent(Buffer buf) {
+		if (buf instanceof ByteBuffer)
+			return 0;
+		else if (buf instanceof ShortBuffer || buf instanceof CharBuffer)
+			return 1;
+		else if (buf instanceof FloatBuffer || buf instanceof IntBuffer)
+			return 2;
+		else if (buf instanceof LongBuffer || buf instanceof DoubleBuffer)
+			return 3;
+		else
+			throw new IllegalStateException("Unsupported buffer type: " + buf);
+	}
+
+	public static int getOffset(Buffer buffer) {
+		return buffer.position() << getElementSizeExponent(buffer);
 	}
 }

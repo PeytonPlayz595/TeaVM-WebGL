4ed964523ec4fc3c0a0f7c8e48ea9143a3d12f24
JessePinkman27 <114885346+JessePinkman27@users.noreply.github.com>
2023-11-27 11:28:53 +0000

    Move BufferedImage to its own class

--- src/main/java/org/lwjgl/opengl/DisplayMode.java
+++ src/main/java/org/lwjgl/opengl/DisplayMode.java
@@ -117,44 +117,4 @@ public class DisplayMode {
 
 	@JSBody(params = { "url" }, script = "URL.revokeObjectURL(url);")
 	private static native void freeDataURL(String url);
-
-
-    private static class BufferedImage {
-    
-        final int[] data;
-        final int w;
-        final int h;
-        final boolean alpha;
-    
-        BufferedImage(int pw, int ph, boolean palpha) {
-            this.w = pw;
-            this.h = ph;
-            this.alpha = palpha;
-            this.data = new int[pw * ph];
-        }
-    
-        BufferedImage(int[] pdata, int pw, int ph, boolean palpha) {
-            if (pdata.length != pw * ph) {
-                throw new IllegalArgumentException("array size does not equal image size");
-            }
-            this.w = pw;
-            this.h = ph;
-            this.alpha = palpha;
-            if (!palpha) {
-                for (int i = 0; i < pdata.length; ++i) {
-                    pdata[i] = pdata[i] | 0xFF000000;
-                }
-            }
-            this.data = pdata;
-        }
-    
-        BufferedImage getSubImage(int x, int y, int pw, int ph) {
-            int[] img = new int[pw * ph];
-            for (int i = 0; i < ph; ++i) {
-                System.arraycopy(data, (i + y) * this.w + x, img, i * pw, pw);
-            }
-            return new BufferedImage(img, pw, ph, alpha);
-        }
-    
-    }
 }
